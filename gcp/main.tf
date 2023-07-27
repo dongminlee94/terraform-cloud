5e36b9882eaeadf3ea36d7606ee97a89a7a2b905
dongminlee94 <kid33629@gmail.com>
2023-07-27 17:27:38 +0900

    FEAT: add gce instance

--- gcp/main.tf
+++ gcp/main.tf
@@ -27,3 +27,29 @@ resource "google_compute_subnetwork" "tf_asne1_prod_1" {
     region        = "asia-northeast1"
     network       = google_compute_network.tf_prod_1.id
 }
+
+resource "google_service_account" "gce" {
+    account_id   = "google-compute-engine"
+    display_name = "google-compute-engine"
+}
+
+resource "google_compute_instance" "tf_asne1_prod_1" {
+    name         = "tf-asne1-prod-1"
+    machine_type = "e2-medium"
+    zone         = "asia-northeast1-a"
+
+    boot_disk {
+        initialize_params {
+            image = "ubuntu-os-cloud/ubuntu-2004-focal-v20230724"
+        }
+    }
+
+    network_interface {
+        subnetwork = google_compute_subnetwork.tf_asne1_prod_1.id
+    }
+
+    service_account {
+        email  = google_service_account.gce.email
+        scopes = ["cloud-platform"]
+    }
+}
