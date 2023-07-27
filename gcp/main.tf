7fa6d33a40b4f82e4e54b6c7f074134bd5f9a2cb
dongminlee94 <kid33629@gmail.com>
2023-07-27 17:26:40 +0900

    FEAT: add subnet network

--- gcp/main.tf
+++ gcp/main.tf
@@ -20,3 +20,10 @@ resource "google_compute_network" "tf_prod_1" {
     name                    = "tf-prod-1"
     auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "tf_asne1_prod_1" {
+    name          = "tf-asne1-prod-1"
+    ip_cidr_range = "10.1.0.0/16"
+    region        = "asia-northeast1"
+    network       = google_compute_network.tf_prod_1.id
+}
