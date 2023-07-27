60df7138f0861ad60a65fbb50fce863349f5619a
dongminlee94 <kid33629@gmail.com>
2023-07-27 17:52:33 +0900

    FEAT: add external static ip

--- gcp/main.tf
+++ gcp/main.tf
@@ -16,16 +16,16 @@ provider "google" {
     project     = var.project
 }
 
-resource "google_compute_network" "tf_prod_1" {
-    name                    = "tf-prod-1"
+resource "google_compute_network" "tf_gcp" {
+    name                    = "tf-gcp"
     auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "tf_asne1_prod_1" {
-    name          = "tf-asne1-prod-1"
+resource "google_compute_subnetwork" "tf_gcp_prod" {
+    name          = "tf-gcp-prod"
     ip_cidr_range = "10.1.0.0/16"
     region        = "asia-northeast1"
-    network       = google_compute_network.tf_prod_1.id
+    network       = google_compute_network.tf_gcp.id
 }
 
 resource "google_service_account" "gce" {
@@ -33,8 +33,8 @@ resource "google_service_account" "gce" {
     display_name = "google-compute-engine"
 }
 
-resource "google_compute_instance" "tf_asne1_prod_1" {
-    name         = "tf-asne1-prod-1"
+resource "google_compute_instance" "tf_gcp_prod" {
+    name         = "tf-gcp-prod"
     machine_type = "e2-medium"
     zone         = "asia-northeast1-a"
 
@@ -45,11 +45,20 @@ resource "google_compute_instance" "tf_asne1_prod_1" {
     }
 
     network_interface {
-        subnetwork = google_compute_subnetwork.tf_asne1_prod_1.id
+        subnetwork = google_compute_subnetwork.tf_gcp_prod.id
+
+        access_config {
+            nat_ip = google_compute_address.tf_gcp_prod.address
+        }
     }
 
     service_account {
         email  = google_service_account.gce.email
         scopes = ["cloud-platform"]
     }
+}
+
+resource "google_compute_address" "tf_gcp_prod" {
+    name   = "tf-gcp-prod"
+    region = "asia-northeast1"
 }
