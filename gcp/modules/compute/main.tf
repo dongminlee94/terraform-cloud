a1b1d82b1bf6d6a2e2799e1d58790b342bcfe2f0
dongminlee94 <kid33629@gmail.com>
2023-07-31 01:30:26 +0900

    FEAT: add vm instance

--- gcp/modules/compute/main.tf
+++ gcp/modules/compute/main.tf
@@ -28,3 +28,34 @@ resource "google_compute_instance" "tf_gcp_prod" {
     ssh-keys = "tf-gcp-prod:${base64decode(var.ssh_pub_key)}"
   }
 }
+
+resource "google_compute_instance" "tf_gcp_prod_2" {
+  name         = "tf-gcp-prod-2"
+  machine_type = "e2-medium"
+  zone         = "asia-northeast1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-focal-v20230724"
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnetwork.id
+
+    access_config {
+      nat_ip = var.nat_ip.address
+    }
+  }
+
+  service_account {
+    email  = var.service_account.email
+    scopes = ["cloud-platform"]
+  }
+
+  tags = ["allow-http", "allow-ssh"]
+
+  metadata = {
+    ssh-keys = "tf-gcp-prod:${base64decode(var.ssh_pub_key)}"
+  }
+}
