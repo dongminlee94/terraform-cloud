2914e75c37d9abc23d4eb3267b7c17d2cde17a92
dongminlee94 <kid33629@gmail.com>
2023-09-03 02:18:48 +0900

    fix: update sa name

--- gcp/global/iam/main.tf
+++ gcp/global/iam/main.tf
@@ -1,6 +1,6 @@
 module "service_account_compute" {
   source = "../../modules/iam"
 
-  account_id   = "sa_compute"
-  display_name = "sa_compute"
+  account_id   = "sa-compute"
+  display_name = "sa-compute"
 }
