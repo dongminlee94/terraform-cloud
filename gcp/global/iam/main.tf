72cf0614777af5d247b7a5543dce7e36bb769a62
dongminlee94 <kid33629@gmail.com>
2023-09-03 02:41:44 +0900

    feat: remove sa

--- gcp/global/iam/main.tf
+++
@@ -1,6 +0,0 @@
-module "service_account_global" {
-  source = "../../modules/iam"
-
-  account_id   = "sa-global"
-  display_name = "sa-global"
-}
--- gcp/modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "account_id" {
-  type        = string
-  description = "Service Account id"
-}
-
-variable "display_name" {
-  type        = string
-  description = "Service Account display name"
-}
