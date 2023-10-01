482f905d79b45c2a571ff346fd7b304bc20e3654
dongminlee94 <kid33629@gmail.com>
2023-10-02 03:09:41 +0900

    fix: resolve vpc_cidr_block error

--- aws/global/vpc/main.tf
+++ aws/global/vpc/main.tf
@@ -2,7 +2,7 @@ module "vpc_global" {
   source = "../../modules/vpc"
 
   vpc_name       = "vpc_global"
-  vpc_cidr_block = "10.0.1.0/16"
+  vpc_cidr_block = "10.0.0.0/16"
 
   subnet_name       = "subnet_global"
   subnet_cidr_block = "10.0.1.0/24"
