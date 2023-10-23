a219992f7d4478a871f7a886f9c113089675b4f2
dongminlee94 <kid33629@gmail.com>
2023-10-23 23:33:03 +0900

    feat: remove azure iam

--- azure/modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "display_name" {
-  description = "Azure application display name"
-  type        = string
-}
-
-variable "role_definition_name" {
-  description = "Azure role assignment definition name"
-  type        = string
-}
--- azure/prod/iam/main.tf
+++
@@ -1,6 +0,0 @@
-module "iam" {
-  source = "../../modules/iam"
-
-  display_name         = "devops"
-  role_definition_name = "Contributor"
-}
