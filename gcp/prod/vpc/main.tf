b61c2f9d2838779e78b3e17df6b2f63db410ad07
dongminlee94 <kid33629@gmail.com>
2023-12-08 01:43:18 +0900

    feat(gcp): update ip cidr range

--- gcp/prod/vpc/main.tf
+++ gcp/prod/vpc/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   network_auto_create_subnetworks = false
 
   subnetwork_name                   = "subnetwork"
-  subnetwork_ip_cidr_range          = "10.1.0.0/16"
-  subnetwork_pods_ip_cidr_range     = "10.1.64.0/18"
-  subnetwork_services_ip_cidr_range = "10.1.0.0/20"
+  subnetwork_ip_cidr_range          = "10.0.0.0/24"
+  subnetwork_pods_ip_cidr_range     = "10.0.2.0/24"
+  subnetwork_services_ip_cidr_range = "10.0.4.0/24"
 }
