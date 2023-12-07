b666597e3893e69188cf8e35b5a2bb22d5b6923d
dongminlee94 <kid33629@gmail.com>
2023-12-08 01:34:37 +0900

    feat(gcp): update secondary_ip_range names

--- gcp/modules/vpc/main.tf
+++ gcp/modules/vpc/main.tf
@@ -9,12 +9,12 @@ resource "google_compute_subnetwork" "subnetwork" {
   network       = google_compute_network.network.id
 
   secondary_ip_range {
-    range_name    = "GKE Pods"
+    range_name    = "gke pods"
     ip_cidr_range = var.subnetwork_pods_ip_cidr_range
   }
 
   secondary_ip_range {
-    range_name    = "GKE Services"
+    range_name    = "gke services"
     ip_cidr_range = var.subnetwork_services_ip_cidr_range
   }
 }
